9006fba50a44afbd7772aaaff1203194da6ca9ff
ilnurchikk <152897927+ilnurchikk@users.noreply.github.com>
2024-02-07 18:43:02 +0300

    Add files via upload

---
+++ domfev3js/labfev3js.js
@@ -0,0 +1,48 @@
+console.log("Hello world") 
+   
+let perem;
+perem ="gava"
+let num = 4;
+let text = "text";
+console.log(perem)
+console.log(firstLetter)
+
+///////////////////////111111111111111111
+let userLogin;
+let userName;
+let userCreditCard;
+let maximumDataTransferSpeed
+////////////////////////////22222222222222
+let dayOfWeek
+ ///взаимодествие консоли /////////////3333333333333333
+   ///x = 2 + 6 / (1 + 2 )
+
+///например, х=0 — истина, х=3 — 
+    ///истина, х=9 — истина, х=–1 — ложь, х=10 — ложь).44444444444444444444
+let x
+x < 9 && x > 0  
+x = 10
+console.log(x < 9 && x > 0) 
+let y
+y < 9 && y > 0  
+y = 1
+console.log(y < 9 && y > 0) 
+
+////////////////////5555555555555555
+let c 
+c % 2 == 0 && (c <= 10 && c >= 1  )
+c = 10
+console.log(c % 2 == 0 && (c <= 10 && c >= 1  ))
+
+  
+c % 2 == 0 && (c <= 10 && c >= 1  )
+c = 1
+console.log(c % 2 == 0 && (c <= 10 && c >= 1  ))
+
+//////////////////////66666666666666666666
+typeof 2
+console.log(typeof 2)
+
+console.log(typeof (1 + true))
+console.log(typeof (1+2))
+console.log(typeof (1 + false))
